72b55888e5ab897c647fc56f4b4fe9643865de58
Steve Schofield <steve.schofield@gmail.com>
2025-12-26 22:39:48 -0500

    logs example

---
+++ z_shannon-results-20251226-215728/repos/192.168.1.130/config/index.js
@@ -0,0 +1,9 @@
+/**
+ * Configuration - LSG v2 Generated
+ */
+
+module.exports = {
+  port: process.env.PORT || 3000,
+  nodeEnv: process.env.NODE_ENV || 'development',
+  jwtSecret: process.env.JWT_SECRET || 'change-in-production',
+};
